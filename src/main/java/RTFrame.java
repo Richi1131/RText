c09080f0dc4ef51e441b77a1d91308a4d6db80e9
Richard Stengel <richard.stengel@sap.com>
2025-05-09 11:44:07 +0200

    added menubar

--- src/main/java/RTFrame.java
+++ src/main/java/RTFrame.java
@@ -2,8 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RTFrame extends JFrame {
+    private JTextArea textArea;
+    private JMenuBar menuBar;
+
     public RTFrame() {
-        this.add(new TextArea());
+        textArea = new JTextArea();
+
+        menuBar = new JMenuBar();
+        menuBar.add(new JMenu("File"));
+        menuBar.getMenu(0).add("New");
+        menuBar.getMenu(0).add("Open");
+        menuBar.getMenu(0).add("Save");
+        this.add(textArea);
+        this.add(menuBar, BorderLayout.NORTH);
         this.setSize(1200,800);
     }
 }
