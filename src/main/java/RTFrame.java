4164dff75c063e4256b3c94087689a586aed898a
Richard Stengel <richard.stengel@sap.com>
2025-05-09 11:33:03 +0200

    initial commit

---
+++ src/main/java/RTFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RTFrame extends JFrame {
+    public RTFrame() {
+        this.add(new TextArea());
+        this.setSize(1200,800);
+    }
+}
